e63768ef9c15e4a35279ee4fe6a047068b2e8d71
Lalit Kumar Bhasin <labhas@microsoft.com>
2025-02-17 22:45:23 +0000

    Fix

--- bpf/uprober.bpf.c
+++ bpf/uprober.bpf.c
@@ -3,7 +3,7 @@
 #include <bpf/bpf_helpers.h>
 
 SEC("uprobe/test_function")
-int uprobe_readline(struct pt_regs *ctx) {
+int uprobe_test_function(struct pt_regs *ctx) {
     bpf_printk("Uprobe hit: test_function()\n");
     return 0;
 }
